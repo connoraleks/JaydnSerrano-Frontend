416506c187c8d465e0c90b71c026bd31e3aa946c
connoraleks <connoraleks@gmail.com>
2022-06-12 16:08:14 -0400

    bug: test successful, borders fixed on ios and macos

--- frontend/src/components/Gallery.jsx
+++ frontend/src/components/Gallery.jsx
@@ -8,15 +8,15 @@ const Gallery = () => {
                     <img className={img} src={process.env.PUBLIC_URL + '/assets/gallery/1.jpg'} alt="gallery 1"></img>
                     <h1 style={{transition: "all 0.5s ease"}} className={title}>People</h1>
                 </button>
-                <button className={wrapper} style={{transition: "all 0.5s ease"}} >
+                <button className={wrapper} style={{transition: "all 0.5s ease", filter: "blur(0px)", WebKitFilter: "blur(0px)"}} >
                     <img className={img} src={process.env.PUBLIC_URL + '/assets/gallery/4.jpg'} alt="gallery 2"></img>
                     <h1 style={{transition: "all 0.5s ease"}} className={title}>Cars</h1>
                 </button>
-                <button className={wrapper} style={{transition: "all 0.5s ease"}} >
+                <button className={wrapper} style={{transition: "all 0.5s ease", filter: "blur(0px)", WebKitFilter: "blur(0px)"}} >
                     <img className={img} src={process.env.PUBLIC_URL + '/assets/gallery/5.jpg'} alt="gallery 3"></img>
                     <h1 style={{transition: "all 0.5s ease"}} className={title}>Places</h1>
                 </button>
-                <button className={wrapper} style={{transition: "all 0.5s ease"}} >
+                <button className={wrapper} style={{transition: "all 0.5s ease", filter: "blur(0px)", WebKitFilter: "blur(0px)"}} >
                     <img className={img} src={process.env.PUBLIC_URL + '/assets/gallery/10.jpg'} alt="gallery 4"></img>
                     <h1 style={{transition: "all 0.5s ease"}} className={title}>Nature</h1>
                 </button>
