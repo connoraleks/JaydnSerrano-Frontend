c17fdfa4cf7f665ec9d6406cda869bcd55299dba
connoraleks <connoraleks@gmail.com>
2022-06-12 15:53:59 -0400

    bug: formatting and sizes

--- frontend/src/components/Gallery.jsx
+++ frontend/src/components/Gallery.jsx
@@ -1,24 +1,24 @@
 const Gallery = () => {
     const img = "";
     const wrapper = "relative h-fit flex justify-center items-center border-2 border-white rounded-2xl overflow-hidden basis-auto md:basis-1/2 lg:basis-1/3 group";
-    const section = "group-hover:h-full group-hover:scale-150 absolute bottom-0 bg-gray-600 bg-opacity-50 backdrop-blur-lg w-full h-1/6 flex justify-center items-center text-xs md:text-sm lg:text-base text-gray-200";
+    const title = "group-hover:h-full group-hover:scale-150 absolute bottom-0 bg-gray-600 bg-opacity-50 backdrop-blur-lg w-full h-1/6 flex justify-center items-center text-auto text-gray-200 overflow-hidden";
     return (
         <div className="h-fit gap-8 flex flex-wrap justify-center items-center p-8">
                 <button className={wrapper} style={{transition: "all 0.5s ease"}} >
                     <img className={img} src={process.env.PUBLIC_URL + '/assets/gallery/1.jpg'} alt="gallery 1"></img>
-                    <h1 style={{transition: "all 0.5s ease"}} className={section}>People</h1>
+                    <h1 style={{transition: "all 0.5s ease"}} className={title}>People</h1>
                 </button>
                 <button className={wrapper} style={{transition: "all 0.5s ease"}} >
                     <img className={img} src={process.env.PUBLIC_URL + '/assets/gallery/4.jpg'} alt="gallery 2"></img>
-                    <h1 style={{transition: "all 0.5s ease"}} className={section}>Cars</h1>
+                    <h1 style={{transition: "all 0.5s ease"}} className={title}>Cars</h1>
                 </button>
                 <button className={wrapper} style={{transition: "all 0.5s ease"}} >
                     <img className={img} src={process.env.PUBLIC_URL + '/assets/gallery/5.jpg'} alt="gallery 3"></img>
-                    <h1 style={{transition: "all 0.5s ease"}} className={section}>Places</h1>
+                    <h1 style={{transition: "all 0.5s ease"}} className={title}>Places</h1>
                 </button>
                 <button className={wrapper} style={{transition: "all 0.5s ease"}} >
                     <img className={img} src={process.env.PUBLIC_URL + '/assets/gallery/10.jpg'} alt="gallery 4"></img>
-                    <h1 style={{transition: "all 0.5s ease"}} className={section}>Nature</h1>
+                    <h1 style={{transition: "all 0.5s ease"}} className={title}>Nature</h1>
                 </button>
             </div>
     );
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,13 +1,13 @@
 const Home = () => {
     return (
-        <div className="w-full h-screen flex flex-col xl:flex-row justify-between xl:justify-center">
-            <div className="flex flex-col gap-2 justify-center items-start px-8">
+        <div className="w-full h-screen overflow-hidden relative">
+            <div className="h-1/3 lg:w-3/5 absolute mx-auto left-0 right-0 lg:ml-0 bottom-0 top-0 mt-8 lg:my-auto px-12 flex flex-col gap-2 z-10">
                 <div className="text-center md:text-left w-full">
                     <h1 className="text-4xl md:text-6xl my-4 font-bold text-white" style={{transition: "all 0.5s ease"}} >Jaydn Serrano Photography</h1>
                     <p className="text-md md:text-2xl text-gray-200 mb-4 ml-2" style={{transition: "all 0.5s ease"}} >Don't lose track of the finer details.</p>
                 </div>
             </div>
-            <img className="" src={process.env.PUBLIC_URL + '/assets/home.png'}  alt="jaydn headshot"/>
+            <img style={{transition: "all 0.55s ease"}} className="h-2/3 lg:h-screen absolute mx-auto left-0 right-0 lg:mr-0 bottom-0 z-0" src={process.env.PUBLIC_URL + '/assets/home.png'}  alt="jaydn headshot"/>
         </div>
     );
 }
