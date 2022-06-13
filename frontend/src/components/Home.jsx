7bf151444cf36930eb97d18c0b07ec832f8ccedd
connoraleks <connoraleks@gmail.com>
2022-06-12 21:51:00 -0400

    bug: again

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,11 @@
 const Home = ({home}) => {
     return (
-        <div ref={home} className="h-screen overflow-hidden flex flex-col justify-between items-end xl:justify-center xl:flex-row">
-            <div className="w-full h-full flex flex-col items-center md:items-start md:justify-center text-center md:text-left p-4" style={{transition: "all 0.55s ease"}}>
+        <div ref={home} className="h-screen flex flex-col xl:flex-row justify-between mt-16 md:m-16">
+            <div className="w-full text-center md:text-left lg:my-auto lg:pb-16" style={{transition: "all 0.55s ease"}}>
                 <h1 className="text-4xl lg:text-5xl xl:text-6xl my-4 font-bold text-white" style={{transition: "all 0.5s ease"}} >Jaydn Serrano Photography</h1>
                 <p className="text-md lg:text-lg xl:text-xl text-gray-200" style={{transition: "all 0.5s ease"}} >Don't lose track of the finer details.</p>
             </div>
-            <div className="w-fit h-fit flex justify-center"><img className="" style={{transition: "all 0.55s ease", maxWidth: "85%"}} src={process.env.PUBLIC_URL + '/assets/home.png'}  alt="jaydn headshot"/></div>
+            <img className="w-max" style={{transition: "all 0.55s ease"}} src={process.env.PUBLIC_URL + '/assets/home.png'}  alt="jaydn headshot"/>
         </div>
     );
 }
