3ea5d6eee31be452c9bb16a3dc064615946ed1d0
connoraleks <connoraleks@gmail.com>
2022-06-12 20:23:15 -0400

    bug: proportions

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = ({home}) => {
                 <h1 className="text-4xl lg:text-5xl xl:text-6xl my-4 font-bold text-white w-fit" style={{transition: "all 0.5s ease"}} >Jaydn Serrano Photography</h1>
                 <p className="text-md lg:text-lg xl:text-xl text-gray-200 pr-4 w-fit" style={{transition: "all 0.5s ease"}} >Don't lose track of the finer details.</p>
             </div>
-            <img className="h-3/5 md:h-4/5 lg:h-full w-min absolute -z-10 left-0 right-0 bottom-0 ml-auto mr-4 lg:mr-16" style={{transition: "all 0.55s ease"}} src={process.env.PUBLIC_URL + '/assets/home.png'}  alt="jaydn headshot"/>
+            <img className="h-3/5 md:h-4/5 2xl:h-full w-min absolute -z-10 left-0 right-0 bottom-0 ml-auto mr-4 lg:mr-16" style={{transition: "all 0.55s ease", maxWidth: "85%"}} src={process.env.PUBLIC_URL + '/assets/home.png'}  alt="jaydn headshot"/>
         </div>
     );
 }
