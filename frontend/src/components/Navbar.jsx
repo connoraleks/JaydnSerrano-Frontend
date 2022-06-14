75c5a6f9c1a5f99e7dd9a3e85b309cad83eec286
Connorjaz <connoraz141@gmail.com>
2022-06-14 17:40:36 -0400

    bug: no animation on safari

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import {useState, useEffect, useRef} from "react";
 const Navbar = ({pages}) => {
     const closeButton = useRef(null);
+    const openButton = useRef(null);
     const exit = () => {closeButton.current.focus(); closeButton.current.click();};
     const hideNav = () => setNav("z-20 -translate-x-full h-screen w-screen sm:w-48 flex absolute left-0 flex-col justify-center gap-24 bg-black bg-opacity-50 backdrop-blur-lg box-border text-center");
-    const showNav = () => setNav("z-20 h-screen w-screen sm:w-48 flex absolute left-0 flex-col justify-center gap-24 bg-black bg-opacity-50 backdrop-blur-lg box-border text-center")
+    const showNav = () => {openButton.current.focus(); setNav("z-20 h-screen w-screen sm:w-48 flex absolute left-0 flex-col justify-center gap-24 bg-black bg-opacity-50 backdrop-blur-lg box-border text-center")}
     const [button, setButton] = useState(null);
     const buttonHover = "text-white text-lg lg:text-xl hover:text-blue-500";
     const [nav, setNav] = useState("z-20 -translate-x-full md:translate-x-0 h-screen w-screen sm:w-48 flex absolute left-0 flex-col justify-center gap-24 bg-black bg-opacity-50 backdrop-blur-lg box-border text-center sm:border-r sm:border-white");
@@ -28,7 +29,7 @@ const Navbar = ({pages}) => {
         <div className="absolute top-0 left-0">
         {button && 
         <div className="absolute">
-        <button id="open" class="relative group m-4 z-50 absolute md:hidden" style={{transition: "all 0.5s ease"}}  onClick={showNav}>
+        <button id="open" ref={openButton} class="relative group m-4 z-50 absolute md:hidden" style={{transition: "all 0.5s ease"}}  onClick={showNav}>
             <div class="relative flex overflow-hidden items-center justify-center rounded-full w-[50px] h-[50px] transform transition-all bg-black ring-0 ring-gray-300 ring-opacity-30 duration-200 shadow-md">
             <div class="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-500 origin-center overflow-hidden group-focus:-rotate-180">
                 <div class="bg-white h-[2px] w-7 transform transition-all duration-500 group-focus:rotate-45 -translate-x-1"></div>
