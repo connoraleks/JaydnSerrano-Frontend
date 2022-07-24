286faa8d0b51fdf751215ac002d6d6e18130b004
connoraleks <connoraleks@gmail.com>
2022-07-24 10:30:21 -0400

    feat: Gallery rework for new API and Accordions

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,52 +1,10 @@
-export const photos = [
-  {
-    src: `${process.env.PUBLIC_URL + '/assets/gallery/1.jpg'}`,
-    width: 4,
-    height: 3
-  },
-  {
-    src: `${process.env.PUBLIC_URL + '/assets/gallery/2.jpg'}`,
-    width: 1,
-    height: 1
-  },
-  {
-    src: `${process.env.PUBLIC_URL + '/assets/gallery/3.jpg'}`,
-    width: 3,
-    height: 4
-  },
-  {
-    src: `${process.env.PUBLIC_URL + '/assets/gallery/4.jpg'}`,
-    width: 3,
-    height: 4
-  },
-  {
-    src: `${process.env.PUBLIC_URL + '/assets/gallery/5.jpg'}`,
-    width: 3,
-    height: 4
-  },
-  {
-    src: `${process.env.PUBLIC_URL + '/assets/gallery/6.jpg'}`,
-    width: 4,
-    height: 3
-  },
-  {
-    src: `${process.env.PUBLIC_URL + '/assets/gallery/7.jpg'}`,
-    width: 1,
-    height: 1
-  },
-  {
-    src: `${process.env.PUBLIC_URL + '/assets/gallery/8.jpg'}`,
-    width: 3,
-    height: 4
-  },
-  {
-    src: `${process.env.PUBLIC_URL + '/assets/gallery/9.jpg'}`,
-    width: 3,
-    height: 4
-  },
-  {
-    src: `${process.env.PUBLIC_URL + '/assets/gallery/10.jpg'}`,
-    width: 3,
-    height: 4
-  },
-];
+import axios from 'axios';
+let sections = axios('https://api.jaydnserrano.com/photos')
+  .then(response => {
+    return response;
+  })
+  .catch(error => {
+    console.log(error)
+  }
+  )
+export default sections;
