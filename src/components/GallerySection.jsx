9d849436abb42c4f6d643c5794e9a9d8984fec4a
connoraleks <connoraleks@gmail.com>
2022-08-29 17:20:10 -0400

    V2.0.0: restyled, refitted gallery sections

--- src/components/GallerySection.jsx
+++ src/components/GallerySection.jsx
@@ -1,27 +1,76 @@
 import PhotoAlbum from "react-photo-album";
-import "yet-another-react-lightbox/styles.css";
-
-import Sections from "./Sections";
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useState, useEffect } from 'react';
 import { Accordion, AccordionSummary, AccordionDetails } from './CustomAccordion';
-const GallerySection = ({gallery}) => {
+import axios from 'axios';
+const Gallerysections = ({gallery}) => {
+    const [sections, setSections] = useState(null);
+    const [photos, setPhotos] = useState(null);
+    const [loading, setLoading] = useState(true);
     const [expanded, setExpanded] = useState(false);
-    const [sections, setSections] = useState(new Sections());
     const handleChange = (panel) => (event, isExpanded) => {
         setExpanded(isExpanded ? panel : false);
+        //Scroll to panel when expanded
+        if (isExpanded) {
+            const element = document.getElementById(panel);
+            setTimeout(() => element.scrollIntoView({ behavior: 'smooth' , block: 'start', inline: 'center'}), 750);
+        }
     }
+    const fetchSections = async () => {
+        const res = await axios('https://api.jaydnserrano.com/sections');
+        if(res.data.success) setSections({'names': res.data.sections, 'count': res.data.count});
+    }
+    const fetchPhotos = async () => {
+        const res = await axios('https://api.jaydnserrano.com/photos');
+        if(res.data.success) setPhotos(res.data.photos);
+    }
+
     useEffect(() => {
-        console.log(sections.sections)
-        console.log(sections.section_data)
-    } , []);
+        fetchSections();
+    }, []);
+    useEffect(() => {
+        if(sections) {
+            fetchPhotos();
+        }
+    } , [sections]);
+    useEffect(() => {
+        if(photos) {
+            console.log(photos)
+            setLoading(false);
+        }
+    } , [photos]);
     return (
         <div ref={gallery} className="h-fit text-white my-4">
             {/* Create an accordion for each section in sections */}
-            {/* Inform the user if loading is happening */}
+            {!loading && sections.names.map((section, index) => {
+                return (
+                    <Accordion id={index} expanded={expanded === index} key={index} onChange={handleChange(index)}>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls={`${section}-content`}
+                            id={`${section}-header`}
+                        >
+                            <Typography><span className="font-bold text-2xl">{section}</span> <br/> <span className="text-slate-500">{sections.count[section].num_files + ' photos | ' + sections.count[section].num_subdirectories + ' subcategories'}</span></Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <PhotoAlbum
+                                photos={photos[section]}
+                                layout="rows"
+                                targetRowHeight={350}
+                            />
+                        </AccordionDetails>
+                    </Accordion>
+                )
+            } )}
+            {loading && <div className="text-center">
+                <div className="spinner-border text-white" role="status">
+                    <span className="sr-only">Loading...</span>
+                    </div>
+                </div>
+            }
         </div>
     );
 }
 
-export default GallerySection;
+export default Gallerysections;
