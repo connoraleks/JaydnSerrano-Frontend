80907bf0ccab2e33c49ec9c201cce5e1604c39ef
connoraleks <connoraleks@gmail.com>
2022-09-07 14:10:09 -0400

    feat-in-prog: nested subdirectories

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -2,13 +2,38 @@ import ContactForm from './ContactForm'
 import Home from './Home'
 import GallerySection from './GallerySection'
 import About from './About'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 const Display = ({home,gallery,about,contact}) => {
+    const [sections, setSections] = useState(null);
+    const [photos, setPhotos] = useState(null);
+    const fetchSections = async () => {
+        const res = await axios('https://api.jaydnserrano.com/sections');
+        if(res.data.success) setSections(res.data.sections);
+    }
+    const fetchPhotos = async () => {
+        const res = await axios('https://api.jaydnserrano.com/photos');
+        if(res.data.success) setPhotos(res.data.photos);
+    }
+
+    useEffect(() => {
+        fetchSections();
+    }, []);
+    useEffect(() => {
+        if(sections) {
+            console.log('sections',sections);
+            fetchPhotos();
+        }
+    } , [sections]);
+    useEffect(() => {
+        if(photos) console.log(photos);
+    }, [photos]);
     return (
         <div className="sm:pt-0 md:pl-48 w-full h-full">
             <div className="w-full h-full overflow-y-scroll">
                 <Home home={home}></Home>
                 <About about={about}></About>
-                <GallerySection gallery={gallery}></GallerySection>
+                {sections && <GallerySection galleryref={gallery} directory={sections}></GallerySection>}
                 <ContactForm contact={contact}></ContactForm>
             </div>
         </div>
--- src/components/GallerySection.jsx
+++ src/components/GallerySection.jsx
@@ -1,76 +1,47 @@
-import PhotoAlbum from "react-photo-album";
+//import PhotoAlbum from "react-photo-album";
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useState, useEffect } from 'react';
 import { Accordion, AccordionSummary, AccordionDetails } from './CustomAccordion';
-import axios from 'axios';
-const Gallerysections = ({gallery}) => {
-    const [sections, setSections] = useState(null);
-    const [photos, setPhotos] = useState(null);
-    const [loading, setLoading] = useState(true);
+const GallerySection = ({gallery, directory}) => {
     const [expanded, setExpanded] = useState(false);
     const handleChange = (panel) => (event, isExpanded) => {
+        console.log(panel);
         setExpanded(isExpanded ? panel : false);
-        //Scroll to panel when expanded
-        if (isExpanded) {
-            const element = document.getElementById(panel);
-            setTimeout(() => element.scrollIntoView({ behavior: 'smooth' , block: 'start', inline: 'center'}), 750);
-        }
     }
-    const fetchSections = async () => {
-        const res = await axios('https://api.jaydnserrano.com/sections');
-        if(res.data.success) setSections({'names': res.data.sections, 'count': res.data.count});
-    }
-    const fetchPhotos = async () => {
-        const res = await axios('https://api.jaydnserrano.com/photos');
-        if(res.data.success) setPhotos(res.data.photos);
-    }
-
     useEffect(() => {
-        fetchSections();
-    }, []);
-    useEffect(() => {
-        if(sections) {
-            fetchPhotos();
-        }
-    } , [sections]);
-    useEffect(() => {
-        if(photos) {
-            console.log(photos)
-            setLoading(false);
-        }
-    } , [photos]);
+        console.log(directory.name);
+    }, [directory]);
     return (
         <div ref={gallery} className="h-fit text-white my-4">
-            {/* Create an accordion for each section in sections */}
-            {!loading && sections.names.map((section, index) => {
+            {/* Create an accordion for each section in directory */}
+            {directory && directory.dirs.map((section) => {
                 return (
-                    <Accordion id={index} expanded={expanded === index} key={index} onChange={handleChange(index)}>
+                    <Accordion id={section.id} expanded={expanded === section.id} key={section.id} onChange={handleChange(section.id)}>
                         <AccordionSummary
                             expandIcon={<ExpandMoreIcon />}
-                            aria-controls={`${section}-content`}
-                            id={`${section}-header`}
+                            aria-controls={`${section.name}-content`}
+                            id={`${section.name}-header`}
                         >
-                            <Typography><span className="font-bold text-2xl">{section}</span> <br/> <span className="text-slate-500">{sections.count[section].num_files + ' photos | ' + sections.count[section].num_subdirectories + ' subcategories'}</span></Typography>
+                            <Typography><span className="font-bold text-2xl">{section.name}</span> <br/> <span className="text-slate-500">{section.photos.length+ ' photos | ' + section.dirs.length + ' subcategories'}</span></Typography>
                         </AccordionSummary>
                         <AccordionDetails>
-                            <PhotoAlbum
-                                photos={photos[section]}
-                                layout="rows"
-                                targetRowHeight={350}
-                            />
+                            {/* Create an accordion for each dir in section.dirs[], and then make a Gallerysections for them */}
+                            <GallerySection directory={section}></GallerySection>
+                            {/* Create a PhotoAlbum for each photo in section.photos[] */}
+                            {section.photos.map((photo) => {
+                                return (
+                                    <div key={photo.id}>
+                                        <h1>{photo.name}</h1>
+                                    </div>
+                                )
+                            })}
                         </AccordionDetails>
                     </Accordion>
                 )
             } )}
-            {loading && <div className="text-center">
-                <div className="spinner-border text-white" role="status">
-                    <span className="sr-only">Loading...</span>
-                    </div>
-                </div>
-            }
         </div>
     );
 }
 
-export default Gallerysections;
+export default GallerySection;
