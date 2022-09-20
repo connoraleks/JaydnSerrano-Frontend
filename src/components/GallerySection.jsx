950eb40dbc98d313cfb2c6ff7127d5f43987b978
connoraleks <connoraleks@gmail.com>
2022-09-20 18:05:33 -0400

    style: changed gallery design

--- src/components/GallerySection.jsx
+++ src/components/GallerySection.jsx
@@ -4,42 +4,63 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useState } from 'react';
 import { Accordion, AccordionSummary, AccordionDetails } from './CustomAccordion';
 const GallerySection = ({galleryref, directory}) => {
+    const responsiveRowHeight = {
+        0: 250,
+        500: 350,
+        1000: 450,
+        1500: 550,
+
+
+    };
     const [expanded, setExpanded] = useState(false);
     const handleChange = (panel) => (event, isExpanded) => {
         console.log(panel);
         setExpanded(isExpanded ? panel : false);
     }
     return (
-        <div ref={galleryref} className="h-fit text-white my-4">
-            {/* Create an accordion for each section in directory */}
-            {directory && directory.dirs.map((section) => {
-                return (
-                    <Accordion id={section.id} expanded={expanded === section.id} key={section.id} onChange={handleChange(section.id)}>
-                        <AccordionSummary
-                            expandIcon={<ExpandMoreIcon />}
-                            aria-controls={`${section.name}-content`}
-                            id={`${section.name}-header`}
-                        >
-                            <Typography><span className="font-bold text-2xl">{section.name}</span> <br/> <span className="text-slate-500">{section.photos.length+ ' photos | ' + section.dirs.length + ' subcategories'}</span></Typography>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            {/* GallerySection for this layer */}
-                            <GallerySection directory={section}></GallerySection>
-                            {/* PhotoAlbum for this layer */}
-                            <PhotoAlbum
-                                photos={section.photos}
-                                layout='rows'
-                                targetRowHeight={350}
-                                margin={2}
-                                onClick={ (photo) => {
-                                    // open link in new tab
-                                    window.open(photo.currentTarget.src, '_blank');
-                                }}
-                            />
-                        </AccordionDetails>
-                    </Accordion>
-                )
-            } )}
+        <div className={galleryref != null ? "bg-white p-4" : ''}>
+            <div ref={galleryref} className={galleryref != null ? "h-fit text-white py-8 border-2 border-gray-700 bg-black rounded-2xl" : "h-fit text-white bg-black"}>
+                {galleryref != null && <h3 className="font-bold text-4xl w-full text-center mb-4">Gallery</h3>}
+                {/* Create an accordion for each section in directory */}
+                {directory && directory.dirs.map((section) => {
+                    return (
+                        <Accordion id={section.id} expanded={expanded === section.id} key={section.id} onChange={handleChange(section.id)}>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls={`${section.name}-content`}
+                                id={`${section.name}-header`}
+                            >
+                                <Typography><span className="font-bold text-2xl">{section.name}</span> <br/> <span className="text-slate-500">{section.photos.length+ ' photos | ' + section.dirs.length + ' subcategories'}</span></Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                {/* GallerySection for this layer */}
+                                <GallerySection directory={section}></GallerySection>
+                                {/* PhotoAlbum for this layer */}
+                                <PhotoAlbum
+                                    photos={section.photos}
+                                    layout='rows'
+                                    margin={2}
+                                    targetRowHeight={(containerWidth) => {
+                                        if (containerWidth < 500) {
+                                            return responsiveRowHeight[0];
+                                        } else if (containerWidth < 1000) {
+                                            return responsiveRowHeight[500];
+                                        } else if (containerWidth < 1500) {
+                                            return responsiveRowHeight[1000];
+                                        } else {
+                                            return responsiveRowHeight[1500];
+                                        }
+                                    }}
+                                    onClick={ (photo) => {
+                                        // open link in new tab
+                                        window.open(photo.currentTarget.src, '_blank');
+                                    }}
+                                />
+                            </AccordionDetails>
+                        </Accordion>
+                    )
+                } )}
+            </div>
         </div>
     );
 }
